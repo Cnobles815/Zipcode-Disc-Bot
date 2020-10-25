f4336f90fa8a65a988901d615a94a44f0866e451
Cnobles815 <Cnobles815@gmail.com>
2020-10-25 15:46:51 -0400

    Digestoratored the token

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,18 @@ import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public final class Main {
 
 
 
-    public static void main(final String[] args) throws FileNotFoundException {
+    public static void main(final String[] args) throws IOException {
+        TokenDigesterator digesterator = new TokenDigesterator();
 
-
-        final String token = "Placeholder+++" ;
+        final String token = digesterator.digest() ;
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
 
